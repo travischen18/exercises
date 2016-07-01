8ec538a2fe05c5615f7182004b03cfaba8f4b71b
Travis Chen <tchen96@cs.washington.edu>
2016-07-01 10:09:24 -0700

    add remove duplicate function to linkedlist

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -8,7 +8,30 @@ class LinkedList(object):
 		self.head = head
 		self.tail = head
 
-	def append(nextNode):
+	# Returns a string representation of the list
+	def toString(self):
+		output = ""
+		currNode = head
+		while (currNode != None):
+			output = output + str(currNode.data) + " "
+			currNode = currNode.next
+
+		return output
+
+	def append(self, nextNode):
 		self.tail.next = nextNode
 		self.tail = self.tail.next
+
+	# Removes duplicate nodes in O(n^2) time
+	def removeDups(self):
+		currentNode = head
+		while (currentNode != None):
+			searchNode = currentNode
+			while (searchNode.next != None):
+				if searchNode.next.data == currentNode.data:
+					searchNode.next = searchNode.next.next
+				else:
+					searchNode = searchNode.next
+			currentNode = currentNode.next
+
 		
